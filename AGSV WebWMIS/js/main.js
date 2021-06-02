d89e8ad1812f7814db3d3f0f9fc824316ddf95e5
VillarealEric22 <65070650+VillarealEric22@users.noreply.github.com>
2021-06-02 21:12:26 +0800

    Updated JS for modals
    Updated class names for modal buttons/links. Updated JavaScript with comments. Please add Forms for Modals.

--- AGSV WebWMIS/js/main.js
+++ AGSV WebWMIS/js/main.js
@@ -1,18 +1,46 @@
+// Dropdown for user 
 document.querySelector(".user-wrapper ul li").addEventListener("click", function(){
     this.classList.toggle("active");
   });
 
-var modalBtn = document.querySelector('.logout');
-var modal = document.querySelector('.modal');
-var modalClose = document.querySelector('.close');
-var modalCloseBtn = document.querySelector('.btn-cancel');
+// Get the modal
+var modal = document.getElementsByClassName('modal');
 
-modalBtn.addEventListener('click',function(){
-  modal.classList.add('modal-active');
-});
-modalClose.addEventListener('click',function(){
-  modal.classList.remove('modal-active');
-});
-modalCloseBtn.addEventListener('click',function(){
-  modal.classList.remove('modal-active');
-});
+// Get the button that opens the modal
+var btn = document.getElementsByClassName("modalBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close");
+
+var cancel = document.getElementsByClassName("btn-cancel");
+
+// When the user clicks the button, open the modal 
+btn[0].onclick = function() {
+    modal[0].style.display = "block";
+}
+
+btn[1].onclick = function() {
+  modal[1].style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span[0].onclick = function() {
+    modal[0].style.display = "none";
+}
+
+span[1].onclick = function() {
+    modal[1].style.display = "none";
+}
+
+cancel[0].onclick = function() {
+  modal[0].style.display = "none";
+}
+
+cancel[1].onclick = function() {
+  modal[1].style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
