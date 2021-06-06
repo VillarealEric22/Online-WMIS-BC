0451e8161c4ec2767f0a98824df098a778efe678
VillarealEric22 <65070650+VillarealEric22@users.noreply.github.com>
2021-06-06 15:49:05 +0800

    table pagination
    created new javascript file for table specific javascripts.
    pagination working, but not showing n items out of n entries
    modal for add and edit is buggy, needs rework on the deisgn

--- AGSV WebWMIS/js/main.js
+++ AGSV WebWMIS/js/main.js
@@ -9,38 +9,64 @@ var modal = document.getElementsByClassName('modal');
 // Get the button that opens the modal
 var btn = document.getElementsByClassName("modalBtn");
 
-// Get the <span> element that closes the modal
+// Get the element that closes the modal
 var span = document.getElementsByClassName("close");
-
 var cancel = document.getElementsByClassName("btn-cancel");
 
 // When the user clicks the button, open the modal 
 btn[0].onclick = function() {
     modal[0].style.display = "block";
 }
-
 btn[1].onclick = function() {
-  modal[1].style.display = "block";
+    modal[1].style.display = "block";
+}
+btn[2].onclick = function() {
+    modal[2].style.display = "block";
+}
+btn[3].onclick = function() {
+    modal[3].style.display = "block";
+}
+btn[4].onclick = function() {
+    modal[4].style.display = "block";
+}
+btn[5].onclick = function() {
+    modal[5].style.display = "block";
 }
 // When the user clicks on <span> (x), close the modal
 span[0].onclick = function() {
     modal[0].style.display = "none";
 }
-
 span[1].onclick = function() {
     modal[1].style.display = "none";
 }
-
+span[2].onclick = function() {
+    modal[2].style.display = "none";
+}
+span[3].onclick = function() {
+    modal[3].style.display = "none";
+}
+span[4].onclick = function() {
+    modal[4].style.display = "none";
+}
+span[5].onclick = function() {
+    modal[5].style.display = "none";
+}
+// When the user clicks on cancel, close the modal
 cancel[0].onclick = function() {
-  modal[0].style.display = "none";
+    modal[0].style.display = "none";
 }
-
 cancel[1].onclick = function() {
-  modal[1].style.display = "none";
+    modal[1].style.display = "none";
 }
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+cancel[2].onclick = function() {
+    modal[2].style.display = "none";
+}
+cancel[3].onclick = function() {
+    modal[3].style.display = "none";
+}
+cancel[4].onclick = function() {
+    modal[4].style.display = "none";
+}
+cancel[5].onclick = function() {
+    modal[5].style.display = "none";
 }
