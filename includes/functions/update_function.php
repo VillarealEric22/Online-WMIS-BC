d6b18da6aebd8ae16debf224f11155c38841de9f
VillarealEric22 <65070650+VillarealEric22@users.noreply.github.com>
2021-12-13 07:10:23 +0800

    Update update_function.php

--- includes/functions/update_function.php
+++ includes/functions/update_function.php
@@ -25,24 +25,27 @@ if ($func == "product"){
     // Close connection
     if ($stmt->execute()){
         echo $product_code. "'s record updated successfully";
-        $img = $_FILES['file']['name'];
-        $tmp = $_FILES['file']['tmp_name'];
-        // get uploaded file's extension
-        $ext = strtolower(pathinfo($img, PATHINFO_EXTENSION));
-        // can upload same image using rand function
-        $final_image = rand(1000,1000000).$img;
-        // check's valid format
-        if(in_array($ext, $valid_extensions)) { 
-            $path = $path.strtolower($final_image); 
-            if(move_uploaded_file($tmp,$path)) {
-                echo "Image Uploaded";
-                $pathclean =  str_replace("../../", "", $path);
-                $insert = $con->query("UPDATE products SET `product_img` = '$pathclean' WHERE product_code = '$product_code'");
+        if(isset()){
+            $img = $_FILES['file']['name'];
+            $tmp = $_FILES['file']['tmp_name'];
+            // get uploaded file's extension
+            $ext = strtolower(pathinfo($img, PATHINFO_EXTENSION));
+            // can upload same image using rand function
+            $final_image = rand(1000,1000000).$img;
+            // check's valid format
+            if(in_array($ext, $valid_extensions)) { 
+                $path = $path.strtolower($final_image); 
+                if(move_uploaded_file($tmp,$path)) {
+                    echo "Image Uploaded";
+                    $pathclean =  str_replace("../../", "", $path);
+                    $insert = $con->query("UPDATE products SET `product_img` = '$pathclean' WHERE product_code = '$product_code'");
+                }
+            } 
+            else {
+                echo 'invalid file';
             }
-        } 
-        else {
-            echo 'invalid file';
         }
+        
     } else { 
         echo "Data Not Saved". $con->error;
     }
