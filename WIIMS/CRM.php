40bc6422af082da9c6b229e56c182fdca634a0c3
VillarealEric22 <65070650+VillarealEric22@users.noreply.github.com>
2021-12-12 22:56:18 +0800

    Delete WIIMS directory

--- WIIMS/CRM.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    include('php/includes/header.php');
-    include('php/includes/navbar.php');
-?>
-
-<?php 
-    include('php/includes/footer.php');
-    include('php/includes/scripts.php');
-?>
--- WIIMS/php/includes/scripts.php
+++
@@ -1,3 +0,0 @@
-<script src = "js/main.js"></script>
-<script src = "js/tables.js"></script>
-<script src = "js/charts.js"></script>
