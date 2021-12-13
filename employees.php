b06ac9ef933bca7c0c4b75c015821b818878312f
VillarealEric22 <65070650+VillarealEric22@users.noreply.github.com>
2021-12-14 00:39:39 +0800

    Update employees.php

--- employees.php
+++ employees.php
@@ -53,9 +53,6 @@
                 <ul class = pagination>
                 </ul>
             </div>
-            <div class="table-info">
-                Total rows: 9999
-            </div>
         </div>
     </div>
 </div>
