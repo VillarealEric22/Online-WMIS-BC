93e40b49a9281d352978cc1fd88551d7b89ab88e
VillarealEric22 <65070650+VillarealEric22@users.noreply.github.com>
2021-12-15 03:03:09 +0800

    Update update_function.php

--- includes/functions/update_function.php
+++ includes/functions/update_function.php
@@ -20,7 +20,7 @@ if ($func == "product"){
     $supplier_id = $_POST['supplier_id'];
     $wty = $_POST['wty'];
     
-    $sql = "UPDATE `products` SET `product_name`=?,`manufacturer`=?,`product_type`=?,`color`=?,`item_price`=?,`critical_amt`=?,`rop_min`=?,`ro_categ`=?, `warranty_code`, `description`=?,`supplier_id`= ? WHERE product_code = ?";
+    $sql = "UPDATE `products` SET `product_name`=?,`manufacturer`=?,`product_type`=?,`color`=?,`item_price`=?,`critical_amt`=?,`rop_min`=?,`ro_categ`=?, `warranty_code`=?, `description`=?,`supplier_id`= ? WHERE product_code = ?";
     $stmt = $con->prepare($sql);
     $stmt->bind_param('sssssdisisis', $product_name, $manufacturer, $product_type, $color,  $item_price, $critical, $reorder, $ro_categ, $wty, $desc, $supplier_id, $product_code);
     // Close connection
