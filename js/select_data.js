ddfdb289d73df04d9832681a6e7a6278a787b6ad
VillarealEric22 <65070650+VillarealEric22@users.noreply.github.com>
2021-12-17 19:12:23 +0800

    Update select_data.js

--- js/select_data.js
+++ js/select_data.js
@@ -104,7 +104,7 @@ function categ(){
         dataType:"json",
         success: function(data){
             $.each(data, function(){
-                var insert = "<option class = 'product_type' value='" + this.id + "'>" + this.product_type + "</option>";
+                var insert = "<option class = 'product_type' value='" + this.id + "'>" + this.id + "</option>";
                 $("#product_type").append(insert);
             })
         },
