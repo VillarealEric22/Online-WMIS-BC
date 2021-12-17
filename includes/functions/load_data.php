473a97e3e41d928458d9c9306b90a7c610ba7275
VillarealEric22 <65070650+VillarealEric22@users.noreply.github.com>
2021-12-17 19:01:07 +0800

    Update load_data.php

--- includes/functions/load_data.php
+++ includes/functions/load_data.php
@@ -3,7 +3,7 @@ include('../db.php');
 
 $func = $_POST['func'];
 if ($func == "product"){
-    $sql = "SELECT product_code, product_img, product_name, manufacturer, c.product_type, color, ro_categ, rop_min, critical_amt, item_price, IFNULL(SUM(quantity),0) AS qty FROM products LEFT JOIN whse_items USING (product_code) LEFT JOIN product_category c ON c.id = products.product_code GROUP BY product_code";
+    $sql = "SELECT product_code, product_img, product_name, manufacturer, c.product_type, color, ro_categ, rop_min, critical_amt, item_price, IFNULL(SUM(quantity),0) AS qty FROM products LEFT JOIN whse_items USING (product_code) LEFT JOIN product_category c ON c.id = products.product_type GROUP BY product_code";
     $result = mysqli_query($con,$sql) or die($con->error); //or die($con->error) is for debugging of SQL Query
     while($rows = mysqli_fetch_array($result)){
         $product_img = $rows['product_img'];
