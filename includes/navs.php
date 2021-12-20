6cda8388b29f74af960d76f532060ec4ca9f0c5a
VillarealEric22 <65070650+VillarealEric22@users.noreply.github.com>
2021-12-20 16:04:51 +0800

    Update navs.php

--- includes/navs.php
+++ includes/navs.php
@@ -20,7 +20,13 @@
 <body>
 	<div class = "cointaner">
 		<div class = "navigation">
-			<ul class = "nav-links">
+			<?php
+			session_start();
+
+			if($_SESSION["userrole"] == "Admin")
+			{
+				?>
+				<ul class = "nav-links">
 				<li>
 					<a href = "dashboard.php">
 						<span class = "icon"><i class="fa fa-apple" aria-hidden="true"></i></span>
@@ -34,140 +40,181 @@
 					</a>
 				</li>
 				<li>
-                    <a href="products.php">
-                        <span class = "icon"><i class="fa fa-tags" aria-hidden="true"></i></span>
-                        <span class="title">Products</span>
-                    </a>
-				</li>
+			    <a href="products.php">
+				<span class = "icon"><i class="fa fa-tags" aria-hidden="true"></i></span>
+				<span class="title">Products</span>
+			    </a>
+					</li>
+					<li>
+			    <a href="categories.php">
+				<span class = "icon"><i class="fa fa-tags" aria-hidden="true"></i></span>
+				<span class="title">Categories</span>
+			    </a>
+					</li>
+					<li>
+			    <a href="warranties.php">
+				<span class = "icon"><i class="fa fa-tags" aria-hidden="true"></i></span>
+				<span class="title">Warranties</span>
+			    </a>
+					</li>
+					<li>
+						<a href="inventory.php">
+				<span class = "icon"><i class="fa fa-cubes" aria-hidden="true"></i></span>
+				<span class="title">Inventory</span>
+			    </a>
+					</li>
+					<li>
+						<a href = "sales.php">
+							<span class = "icon"><i class="fa fa-cash-register" aria-hidden="true"></i></span>
+							<span class = "title">Sales</span>
+						</a>
+					</li>
+					<li>
+						<a href = "orders.php">
+							<span class = "icon"><i class="fa fa-shopping-cart" aria-hidden="true"></i></span>
+							<span class = "title">Orders</span>
+						</a>
+					</li>
+					<li>
+						<a href = "orders-completed.php">
+							<span class = "icon"><i class="fa fa-shopping-cart" aria-hidden="true"></i></span>
+							<span class = "title">Orders(completed)</span>
+						</a>
+					</li>
+					<li>
+						<a href = "returns.php">
+							<span class = "icon"><i class="fa fa-repeat" aria-hidden="true"></i></span>
+							<span class = "title">Returns</span>
+						</a>
+					</li>
+					<li>
+						<a href = "pullout.php">
+							<span class = "icon"><i class="fa fa-dolly" aria-hidden="true"></i></span>
+							<span class = "title">Item Pullout</span>
+						</a>
+					</li>
+					<li>
+						<a href = "transfer.php">
+							<span class = "icon"><i class="fa fa-people-carry" aria-hidden="true"></i></span>
+							<span class = "title">Transfer</span>
+						</a>
+					</li>
+					<li>
+						<a href = "transfer-approved.php">
+							<span class = "icon"><i class="fa fa-people-carry" aria-hidden="true"></i></span>
+							<span class = "title">Transfer(Approved)</span>
+						</a>
+					</li>
+					<li>
+						<a href = "transfer-completed.php">
+							<span class = "icon"><i class="fa fa-people-carry" aria-hidden="true"></i></span>
+							<span class = "title">Transfer(Completed)</span>
+						</a>
+					</li>
+					<li>
+						<a href = "warehouse.php">
+							<span class = "icon"><i class="fa fa-warehouse" aria-hidden="true"></i></span>
+							<span class = "title">Warehouse</span>
+						</a>
+					</li>
+					<li>
+						<a href = "whse_items.php">
+							<span class = "icon"><i class="fa fa-warehouse" aria-hidden="true"></i></span>
+							<span class = "title">Warehouse Items</span>
+						</a>
+					</li>
+					<li>
+						<a href = "customers.php">
+							<span class = "icon"><i class="fa fa-user-tag" aria-hidden="true"></i></span>
+							<span class = "title">Customers</span>
+						</a>
+						<div>
+					</li>
+					<li>
+						<a href = "suppliers.php">
+							<span class = "icon"><i class="fa fa-user-tie" aria-hidden="true"></i></span>
+							<span class = "title">Suppliers</span>
+						</a>
+						<div>
+					</li>
+					<li>
+						<a href = "abc_analysis.php">
+							<span class = "icon"><i class="fa fa-book" aria-hidden="true"></i></span>
+							<span class = "title">ABC Analysis</span>
+						</a>
+					</li>
+					<li>
+						<a href = "sales_report.php">
+							<span class = "icon"><i class="fa fa-book" aria-hidden="true"></i></span>
+							<span class = "title">Sales Report</span>
+						</a>
+					</li>
+					<li>
+						<a href = "safety_stock.php">
+							<span class = "icon"><i class="fa fa-book" aria-hidden="true"></i></span>
+							<span class = "title">Safety Stock</span>
+						</a>
+					</li>
+					<li>
+						<a href = "ppi.php">
+							<span class = "icon"><i class="fa fa-book" aria-hidden="true"></i></span>
+							<span class = "title">Product Performance</span>
+						</a>
+					</li>
+					<li>
+						<a href = "users.php">
+							<span class = "icon"><i class="fa fa-user" aria-hidden="true"></i></span>
+							<span class = "title">Users</span>
+						</a>
+					</li>
+					<li>
+						<a href = "employees.php">
+							<span class = "icon"><i class="fa fa-user-friends" aria-hidden="true"></i></span>
+							<span class = "title">Employees</span>
+						</a>
+					</li>
+				</ul>
+				<?php
+			}
+			else if($_SESSION["userrole"] == "Sales"){
+				?>
+				<ul class = "nav-links">
 				<li>
-                    <a href="categories.php">
-                        <span class = "icon"><i class="fa fa-tags" aria-hidden="true"></i></span>
-                        <span class="title">Categories</span>
-                    </a>
-				</li>
-				<li>
-                    <a href="warranties.php">
-                        <span class = "icon"><i class="fa fa-tags" aria-hidden="true"></i></span>
-                        <span class="title">Warranties</span>
-                    </a>
-				</li>
-				<li>
-					<a href="inventory.php">
-                        <span class = "icon"><i class="fa fa-cubes" aria-hidden="true"></i></span>
-                        <span class="title">Inventory</span>
-                    </a>
-				</li>
-				<li>
-					<a href = "sales.php">
-						<span class = "icon"><i class="fa fa-cash-register" aria-hidden="true"></i></span>
-						<span class = "title">Sales</span>
+					<a href = "dashboard.php">
+						<span class = "icon"><i class="fa fa-apple" aria-hidden="true"></i></span>
+						<span class = "title"><h2>Baker's Craft</h2></span>
 					</a>
 				</li>
 				<li>
-					<a href = "orders.php">
-						<span class = "icon"><i class="fa fa-shopping-cart" aria-hidden="true"></i></span>
-						<span class = "title">Orders</span>
+					<a href = "dashboard.php">
+						<span class = "icon"><i class="fa fa-home" aria-hidden="true"></i></span>
+						<span class = "title">Dashboard</span>
 					</a>
 				</li>
 				<li>
-					<a href = "orders-completed.php">
-						<span class = "icon"><i class="fa fa-shopping-cart" aria-hidden="true"></i></span>
-						<span class = "title">Orders(completed)</span>
-					</a>
-				</li>
-				<li>
-					<a href = "returns.php">
-						<span class = "icon"><i class="fa fa-repeat" aria-hidden="true"></i></span>
-						<span class = "title">Returns</span>
-					</a>
-				</li>
-				<li>
-					<a href = "pullout.php">
-						<span class = "icon"><i class="fa fa-dolly" aria-hidden="true"></i></span>
-						<span class = "title">Item Pullout</span>
-					</a>
-				</li>
-				<li>
-					<a href = "transfer.php">
-						<span class = "icon"><i class="fa fa-people-carry" aria-hidden="true"></i></span>
-						<span class = "title">Transfer</span>
-					</a>
-				</li>
-				<li>
-					<a href = "transfer-approved.php">
-						<span class = "icon"><i class="fa fa-people-carry" aria-hidden="true"></i></span>
-						<span class = "title">Transfer(Approved)</span>
-					</a>
-				</li>
-				<li>
-					<a href = "transfer-completed.php">
-						<span class = "icon"><i class="fa fa-people-carry" aria-hidden="true"></i></span>
-						<span class = "title">Transfer(Completed)</span>
-					</a>
-				</li>
-				<li>
-					<a href = "warehouse.php">
-						<span class = "icon"><i class="fa fa-warehouse" aria-hidden="true"></i></span>
-						<span class = "title">Warehouse</span>
-					</a>
-				</li>
-				<li>
-					<a href = "whse_items.php">
-						<span class = "icon"><i class="fa fa-warehouse" aria-hidden="true"></i></span>
-						<span class = "title">Warehouse Items</span>
-					</a>
-				</li>
-				<li>
-					<a href = "customers.php">
-						<span class = "icon"><i class="fa fa-user-tag" aria-hidden="true"></i></span>
-						<span class = "title">Customers</span>
-					</a>
-					<div>
-				</li>
-				<li>
-					<a href = "suppliers.php">
-						<span class = "icon"><i class="fa fa-user-tie" aria-hidden="true"></i></span>
-						<span class = "title">Suppliers</span>
-					</a>
-					<div>
-				</li>
-				<li>
-					<a href = "abc_analysis.php">
-						<span class = "icon"><i class="fa fa-book" aria-hidden="true"></i></span>
-						<span class = "title">ABC Analysis</span>
-					</a>
-				</li>
-				<li>
-					<a href = "sales_report.php">
-						<span class = "icon"><i class="fa fa-book" aria-hidden="true"></i></span>
-						<span class = "title">Sales Report</span>
-					</a>
-				</li>
-				<li>
-					<a href = "safety_stock.php">
-						<span class = "icon"><i class="fa fa-book" aria-hidden="true"></i></span>
-						<span class = "title">Safety Stock</span>
-					</a>
-				</li>
-				<li>
-					<a href = "ppi.php">
-						<span class = "icon"><i class="fa fa-book" aria-hidden="true"></i></span>
-						<span class = "title">Product Performance</span>
-					</a>
-				</li>
-				<li>
-					<a href = "users.php">
-						<span class = "icon"><i class="fa fa-user" aria-hidden="true"></i></span>
-						<span class = "title">Users</span>
-					</a>
-				</li>
-				<li>
-					<a href = "employees.php">
-						<span class = "icon"><i class="fa fa-user-friends" aria-hidden="true"></i></span>
-						<span class = "title">Employees</span>
-					</a>
-				</li>
-			</ul>
+					    <a href="products.php">
+						<span class = "icon"><i class="fa fa-tags" aria-hidden="true"></i></span>
+						<span class="title">Products</span>
+					    </a>
+					</li>
+					<li>
+						<a href = "sales.php">
+							<span class = "icon"><i class="fa fa-cash-register" aria-hidden="true"></i></span>
+							<span class = "title">Sales</span>
+						</a>
+					</li>
+					<li>
+						<a href = "customers.php">
+							<span class = "icon"><i class="fa fa-user-tag" aria-hidden="true"></i></span>
+							<span class = "title">Customers</span>
+						</a>
+						<div>
+					</li>
+				</ul>
+			
+			<?php
+			}
+			?>
 		</div>
 	</div>
 	<div class = "main">
